6681ad07956522d5726f3f418d90e950900665d2
3615Prunelle <elfrasquitofresquito@gmail.com>
2025-10-20 22:17:33 +0200

    Exam R3 L2 Training Fail

--- 0_Training_&_Archives/Exams42/Rank 3/permutations/perm.h
+++ 0_Training_&_Archives/Exams42/Rank 3/permutations/perm.h
@@ -2,7 +2,7 @@
 #define PERM_H
 
 #include <unistd.h>		// read write
-//#include <string.h>	// strlen
+#include <string.h>		// strlen
 #include <stdlib.h>		// malloc calloc realloc free
 #include <stdio.h>		// printf perror puts
 //#include <errno.h>	// perror
